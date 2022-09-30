a24ed9571f887dd28f64a2979a6e844563597fce
macaronij <macaronij@gmail.com>
2022-09-30 11:10:10 -0300

    agregados los pryectos

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,28 @@
-const footer__botonera = document.getElementById("footer__botonera");
+const footer__botonera = document.querySelector(".nav__home");
 footer__botonera.addEventListener("click", ()=> {
     location.reload()
 })
 
-const footer__proyectos = document.getElementById("footer__proyectos");
-footer__proyectos.addEventListener("click", ()=> {
+const botonera__sobremi = document.getElementById("botonera__sobremi_link");
+botonera__sobremi.addEventListener("click", ()=> {
+    let appdata = document.getElementById("appdata");
+    appdata.innerHTML = `<object type="text/html" data="sobremi.html"></object>`
+})
+
+const botonera__educacion = document.getElementById("botonera__educacion_link");
+botonera__educacion.addEventListener("click", ()=> {
+    let appdata = document.getElementById("appdata");
+    appdata.innerHTML = `<object type="text/html" data="educacion.html"></object>`
+})
+
+const botonera__proyectos = document.getElementById("botonera__proyectos_link");
+botonera__proyectos.addEventListener("click", ()=> {
     let appdata = document.getElementById("appdata");
     appdata.innerHTML = `<object type="text/html" data="proyectos.html"></object>`
 })
 
-const footer__contacto = document.getElementById("footer__contacto");
-footer__contacto.addEventListener("click", ()=> {
+const botonera__contacto = document.getElementById("botonera__contacto_link");
+botonera__contacto.addEventListener("click", ()=> {
     let appdata = document.getElementById("appdata");
     appdata.innerHTML = `<object type="text/html" data="contacto.html"></object>`
 })
-
